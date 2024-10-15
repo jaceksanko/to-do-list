8cc1a85efe27d69c0de9a38b32095f35b50d2def
jaceksanko <jacek.sanko@gmail.com>
2024-10-15 11:25:22 +0200

    Send state to local storage

--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -13,23 +13,37 @@ interface TodoStore {
   toggleTodo: (id: string) => void;
 }
 
-const useStore = create<TodoStore>((set) => ({
-  todos: [],
-  addTodo: (name) =>
-    set((state) => ({
-      todos: [
-        ...state.todos,
-        { id: Date.now().toString(), name, isDone: false },
-      ],
-    })),
-  removeTodo: (id) =>
-    set((state) => ({ todos: state.todos.filter((todo) => todo.id !== id) })),
-  toggleTodo: (id) =>
-    set((state) => ({
-      todos: state.todos.map((todo) =>
-        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
-      ),
-    })),
-}));
+const useStore = create<TodoStore>((set) => {
+  const storedTodos = localStorage.getItem("todos");
+  const initialTodos = storedTodos ? JSON.parse(storedTodos) : [];
+
+  return {
+    todos: initialTodos,
+    addTodo: (name) => {
+      set((state) => {
+        const newTodos = [
+          ...state.todos,
+          { id: Date.now().toString(), name, isDone: false },
+        ];
+        localStorage.setItem("todos", JSON.stringify(newTodos));
+        return { todos: newTodos };
+      });
+    },
+    removeTodo: (id) =>
+      set((state) => {
+        const newTodos = state.todos.filter((todo) => todo.id !== id);
+        localStorage.setItem("todos", JSON.stringify(newTodos));
+        return { todos: newTodos };
+      }),
+    toggleTodo: (id) =>
+      set((state) => {
+        const newTodos = state.todos.map((todo) =>
+          todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+        );
+        localStorage.setItem("todos", JSON.stringify(newTodos));
+        return { todos: newTodos };
+      }),
+  };
+});
 
 export default useStore;
